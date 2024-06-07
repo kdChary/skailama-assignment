2ec05d4a1059770c39048e6f626e1aa8753e7189
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-07 21:12:35 +0530

    navbar foledr created

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div className="left">left</div>
+			<div className="right">right</div>
+		</nav>
+	)
+}
+
+export default Navbar
