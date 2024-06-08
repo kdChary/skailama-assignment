60caf1d1df38e922ba2df7dc80e32ad7605d9b7c
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-08 13:31:36 +0530

    minor changes made

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import Profile from './pages/profile/Profile'
 
-
-// import Navbar from './components/navbar/Navbar';
+// import Home from './pages/home/Home'
+// import Profile from './pages/profile/Profile'
+import Projects from './pages/projects/Projects';
 
 /*
+	* add cursor pointer in logo card
 	* create a separate component for path
 	* Do code clean-up in Side bar
-	* also add routing.
+	* Add routing.
+
+	!# => Home
+		* add popup for home.
+		* add state.
+	!# => Projects
+		* create a page with
+			-> navbar
+			-> back btn
+			-> div containing project components
+		* functionality
+			-> on clicking the back btn go to home.
+			-> clicking on project should go to project.
+			-> add animations to ul
+	!# => next
 */
 
 const App = () => {
 	return (
 		<div>
-			<Profile />
+			<Projects />
 		</div>
 	);
 };
