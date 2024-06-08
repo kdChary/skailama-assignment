2f146e53f5d7d6be9f69d54e86602950f28de8ef
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-08 10:37:19 +0530

    navbar created

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { BsBell, BsGear } from 'react-icons/bs';
+// import { BsGear } from "react-icons/fa6";
+
+import './Navbar.css';
 
 const Navbar = () => {
 	return (
 		<nav>
-			<div className="left">left</div>
-			<div className="right">right</div>
+			<div className="left">
+				<img
+					src="https://res.cloudinary.com/dgga8cymk/image/upload/v1717775620/skailama/lama.-logo.png"
+					alt="logo"
+				/>
+				<span>LAMA.</span>
+			</div>
+			
+			<div className="right">
+				<BsGear className="icon spin" />
+
+				<BsBell className="icon" />
+			</div>
 		</nav>
-	)
-}
+	);
+};
 
 export default Navbar
